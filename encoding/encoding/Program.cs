bce6138e72b21b90403120c0fdb91ef6e2643b91
Dan <dantdamgaard@hotmail.com>
2020-05-25 13:07:16 +0200

    upload encodingUTF folder

---
+++ encoding/encoding/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace encoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "halløj med dig";
+
+            byte[] byteStr = Encoding.ASCII.GetBytes(str);
+
+            foreach(byte b in byteStr)
+            {
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
