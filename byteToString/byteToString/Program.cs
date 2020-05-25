208b833637f5a3b8640efcc35074a43bb6f4232b
Dan <dantdamgaard@hotmail.com>
2020-05-25 13:53:32 +0200

    upload byteToString folder

---
+++ byteToString/byteToString/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace byteToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "halløj med dig";
+
+            byte[] myBytes = Encoding.UTF8.GetBytes(str);
+
+            string byteStr = Encoding.UTF8.GetString(myBytes);
+            Console.WriteLine(byteStr);
+        }
+    }
+}
