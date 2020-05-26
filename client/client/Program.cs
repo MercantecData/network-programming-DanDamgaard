db25089dabb94c900c5545d818e47d74319790d4
Dan <dantdamgaard@hotmail.com>
2020-05-26 10:07:53 +0200

    added client program

---
+++ client/client/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient();
+
+            int port = 13356;
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint endPoint = new IPEndPoint(ip, port);
+
+            client.Connect(endPoint);
+
+            NetworkStream stream = client.GetStream();
+
+            string text = "Come to the darkside we have cookies";
+            byte[] buffer = Encoding.UTF8.GetBytes(text);
+
+            stream.Write(buffer, 0, buffer.Length);
+            client.Close();
+        }
+    }
+}
