b9951c335131bf6d146301023b47a1a0a6764e96
Dan <dantdamgaard@hotmail.com>
2020-05-26 13:03:51 +0200

    updated client

--- client/client/Program.cs
+++ client/client/Program.cs
@@ -9,21 +9,39 @@ namespace client
     {
         static void Main(string[] args)
         {
+            // Declar
             TcpClient client = new TcpClient();
-
             int port = 13356;
             IPAddress ip = IPAddress.Parse("127.0.0.1");
             IPEndPoint endPoint = new IPEndPoint(ip, port);
 
-            client.Connect(endPoint);
+            // Concect with target
+            NetworkStream stream = myStream(client, endPoint);
 
-            NetworkStream stream = client.GetStream();
+            // Message
+            byte[] buffer = message();
 
-            string text = "Come to the darkside we have cookies";
-            byte[] buffer = Encoding.UTF8.GetBytes(text);
-
+            // Send message
             stream.Write(buffer, 0, buffer.Length);
             client.Close();
+
+
+
+        }
+
+
+        public static NetworkStream myStream(TcpClient client, IPEndPoint endPoint)
+        {
+            client.Connect(endPoint);
+            NetworkStream stream = client.GetStream();
+            return stream;
+        }
+
+        public static byte[] message()
+        {
+            string text = "Come to the darkside we have cookies";
+            byte[] buffer = Encoding.UTF8.GetBytes(text);
+            return buffer;
         }
     }
 }
