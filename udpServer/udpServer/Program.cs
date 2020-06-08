757d1be39f7951a9c5e714ad9ace7a78a2247fdf
Dan <dantdamgaard@hotmail.com>
2020-06-08 13:18:05 +0200

    upload updClient and udpServer

---
+++ udpServer/udpServer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace udpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000);
+
+            UdpClient client = new UdpClient(endPoint);
+
+            Receiver(client);
+            
+            Console.ReadLine();
+        }
+
+        public static async void Receiver(UdpClient client)
+        {
+            while (true)
+            {
+                UdpReceiveResult result = await client.ReceiveAsync();
+                byte[] buffer = result.Buffer;
+
+                string text = Encoding.UTF8.GetString(buffer);
+                Console.WriteLine("client says: " + text);   
+            }
+            
+        }
+    }
+}
---
+++ updClient/updClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace updClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient client = new UdpClient();
+
+            string text = "hello UPD"; 
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000);
+
+            client.Send(bytes, bytes.Length, endPoint);
+        }
+
+    }
+}
