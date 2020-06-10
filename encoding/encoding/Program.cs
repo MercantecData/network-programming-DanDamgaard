e2e36a7591526f45ff11565b5cb8639be8ce627b
Dan <dantdamgaard@hotmail.com>
2020-06-10 10:33:35 +0200

    cleaned up code and made comments for encoding

--- encoding/encoding/Program.cs
+++ encoding/encoding/Program.cs
@@ -7,10 +7,13 @@ namespace encoding
     {
         static void Main(string[] args)
         {
+            // message to convert to bytes
             string str = "halløj med dig";
 
+            // convert message to bytes
             byte[] byteStr = Encoding.ASCII.GetBytes(str);
 
+            // print out bytes
             foreach(byte b in byteStr)
             {
                 Console.WriteLine(b);
