910e06d920cda793261e4c726d4397436a2e6a62
Dan <dantdamgaard@hotmail.com>
2020-06-10 08:46:01 +0200

    cleaned up code and made comments for updServer

--- udpServer/udpServer/Program.cs
+++ udpServer/udpServer/Program.cs
@@ -9,15 +9,18 @@ namespace udpServer
     {
         static void Main(string[] args)
         {
+            // declair connection
             IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000);
-
             UdpClient client = new UdpClient(endPoint);
 
+            // start getting messages
             Receiver(client);
-            
+
+            // stop the program
             Console.ReadLine();
         }
 
+        // get messages
         public static async void Receiver(UdpClient client)
         {
             while (true)
