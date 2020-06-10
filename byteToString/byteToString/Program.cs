c51c8f4fc719f38e35ce0f1dac5a406b35de95d3
Dan <dantdamgaard@hotmail.com>
2020-06-10 10:29:24 +0200

    cleaned up code and made comments for byteToString

--- byteToString/byteToString/Program.cs
+++ byteToString/byteToString/Program.cs
@@ -7,10 +7,13 @@ namespace byteToString
     {
         static void Main(string[] args)
         {
+            // message to convert to byte
             string str = "halløj med dig";
 
+            // convert message to bytes
             byte[] myBytes = Encoding.UTF8.GetBytes(str);
 
+            // convert bytes to text and print it
             string byteStr = Encoding.UTF8.GetString(myBytes);
             Console.WriteLine(byteStr);
         }
